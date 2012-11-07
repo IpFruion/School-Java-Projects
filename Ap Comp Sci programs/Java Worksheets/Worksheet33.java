ef95bdc4d5469ddee6d7ca6249ed0f6894c088dd
BlackQuantum <lockwood.derrick@gmail.com>
2012-11-07 11:07:32 -0600

    Update Ap Comp Sci programs/Java Worksheets/Worksheet33.java

--- Ap Comp Sci programs/Java Worksheets/Worksheet33.java
+++ Ap Comp Sci programs/Java Worksheets/Worksheet33.java
@@ -11,6 +11,30 @@ public class Worksheet33
 {
     public static void main()
     {
+        //definitions
+        int input;
+        int output;
+        int number_negative = 0;
+        int number_positive = 0;
         Scanner s = new Scanner(System.in);
+        
+        
+        
+        System.out.println("Input a Integer: ");
+        input = s.nextInt();
+   
+        if (input < 0) 
+        {
+            output = input * -1;
+            number_negative = number_negative + 1;
+        }
+        else
+        {
+            output = (input+5) /10 *10;
+            number_positive = number_positive + 1;
+        }
+        
+        System.out.println("Original input: " +input+ " Output value is: " +output);
+        
     }
 }
