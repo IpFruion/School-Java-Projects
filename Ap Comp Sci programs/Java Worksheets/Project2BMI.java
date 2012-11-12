1206e86e50ad1216560723b93af3574a5d167fcf
Derrick Lockwood <lockwood.derrick@gmail.com>
2012-11-12 10:07:50 -0600

    update to second project

---
+++ Ap Comp Sci programs/Java Worksheets/Project2BMI.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class Project2BMI here.
+ * 
+ * @author Derrick Lockwood
+ * @version 11-11-12
+ */
+public class Project2BMI
+{
+    /**
+     * Design:
+     * Needs to include:
+     * -the website where I got the BMI Info
+     * -the formula you will use to compute BMI
+     * -the names & bounds of the 4 Weight status categories
+     * -All data definitions for both variables & constants you will use
+     * -how you will compute the info telling the user modify their BMI
+     * -the pseudo code for your program
+     * 
+     * Start of Design:
+     * -start at main method
+     * -define data
+     * --input data: height(inches), weight(lbs)
+     * --constant data: status
+     * -start at main
+     * -prompt for and assign input to height and weight input
+     * -run the math to compute the BMI
+     * -check the BMI with the status consants to determine the status of the BMI
+     * -
+     */
+    public static void main()
+    {
+        
+    }
+}
