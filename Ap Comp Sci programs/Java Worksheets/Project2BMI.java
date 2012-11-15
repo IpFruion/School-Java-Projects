5a8aa4c148aa8fc649c4f774133ed4dfecc9f5d5
BlackQuantum <lockwood.derrick@gmail.com>
2012-11-15 11:06:19 -0600

    Update Ap Comp Sci programs/Java Worksheets/Project2BMI.java

--- Ap Comp Sci programs/Java Worksheets/Project2BMI.java
+++ Ap Comp Sci programs/Java Worksheets/Project2BMI.java
@@ -14,8 +14,8 @@ public class Project2BMI
      
      //start at main
      public static void main(){
-         //data: height(inches), weight(lbs), answerbmi, suggestedweight, scanin
-         int weight, answerbmi, suggestedweight;
+         //data: height(inches), weight(lbs), answerbmi, suggestedweightMin, scanin, suggestedweightMax, suggestedweight
+         double weight, answerbmi, suggestedweightMin, suggestedweightMax, suggestedweight;
          Scanner scanin = new Scanner(System.in);
          double height;
         //constant data: UNDERWEIGHT = 18.5, NORMAL = 24.9, OVERWEIGHT = 29.9, OBESE = 30.0, BMI_CONSTANT = 703, HEAVIEST_PERSON = 1400, TALLEST_PERSON = 100, LIGHTEST_PERSON = 50, SHORTEST_PERSON = 1
@@ -23,9 +23,9 @@ public class Project2BMI
         final double NORMAL = 24.9;
         final double OVERWEIGHT = 29.9;
         final double OBESE = 30.0;
-        final int BMI_CONSTANT = 703;
-        final int HEAVIEST_PERSON = 1400;
-        final int LIGHTEST_PERSON = 50;
+        final double BMI_CONSTANT = 703.;
+        final double HEAVIEST_PERSON = 1400.;
+        final double LIGHTEST_PERSON = 50.;
         final double TALLEST_PERSON = 100.;
         final double SHORTEST_PERSON = 1.;
         //start while loop << to break out of this when input = zero
@@ -36,7 +36,7 @@ public class Project2BMI
             System.out.println("Please enter your height(in)(ex. 5.9ft -> 70.8in): ");
             height = scanin.nextDouble();
             System.out.println("Please enter your weight(lbs)(ex. 120lbs): ");
-            weight = scanin.nextInt();
+            weight = scanin.nextDouble();
             //check if zero
             if (height == 0 || weight == 0) {
                 System.out.println("Exiting Program \n");
@@ -44,36 +44,52 @@ public class Project2BMI
                 return;
             }
             //check for the correctness of the input
-                //if not SHORTEST_PERSON<height<TALLEST_PERSON
+                //if not SHORTEST_PERSON<height or not height<TALLEST_PERSON
             if (!(SHORTEST_PERSON<height) || !(height<TALLEST_PERSON)){
                 System.out.println("Not within height range");
                 //break out of closest loop   
                 break;
             }
-                //if not LIGHTEST_PERSON<weight<HEAVIEST_PERSON
+                //if not LIGHTEST_PERSON<weight or not weight<HEAVIEST_PERSON
             if (!(LIGHTEST_PERSON<weight) || !(weight<HEAVIEST_PERSON)){
-                System.out.println("Not within weight range");
+                System.out.println("Not within weight range\n");
                 //break out of closest loop
                 break;
             }
-     //run the math to compute the BMI
-        //answerbmi = (height * height * BMI_CONSTANT) / (weight * BMI_CONSTANT)
-            //answerbmi = (height*height * BMI_CONSTANT) / (weight * BMI_CONSTANT);
-     //run the math to compute the suggested weight
-        //suggestedweightMin = height*height*UNDERWEIGHT/BMI_CONSTANT
-        //suggestedweightMax = height*height*NORMAL/BMI_CONSTANT
-     //check the BMI with the Bounds to determine the status of the BMI
-        //if answerbmi < UNDERWEIGHT
-            //Output "Underweight" and suggestedweightMin - weight
-        //if UNDERWEIGHT < answerbmi < NORMAL
-            //Output "Normal"
-        //if NORMAL < answerbmi < OVERWEIGHT
-            //Output "Overweight" and weight - suggestedweightMax
-        //if answerbmi > OBESE
-            //Output "Obese" and weight - suggestedweightMax
-     //end while loop
+            //run the math to compute the BMI
+            //answerbmi = (height * height * BMI_CONSTANT) / (weight * BMI_CONSTANT)
+            answerbmi = height * height * BMI_CONSTANT / (weight * BMI_CONSTANT);
+            //run the math to compute the suggested weight
+            //suggestedweightMin = height*height*UNDERWEIGHT/BMI_CONSTANT
+            suggestedweightMin = height*height*UNDERWEIGHT/BMI_CONSTANT;
+            //suggestedweightMax = height*height*NORMAL/BMI_CONSTANT
+            suggestedweightMax = height*height*NORMAL/BMI_CONSTANT;
+            //check the BMI with the Bounds to determine the status of the BMI
+            //if answerbmi < UNDERWEIGHT
+            if (answerbmi < UNDERWEIGHT)
+            {
+                //Output "Underweight" and suggestedweightMin - weight
+                suggestedweight = suggestedweightMin - weight;
+                System.out.println("Underweight:\nYour BMI is: " +answerbmi+"\n You need to gain: "+suggestedweight+"lbs\n");
             }
-     //end while loop
+            //if UNDERWEIGHT < answerbmi < NORMAL
+            if (UNDERWEIGHT < answerbmi || answerbmi < NORMAL)
+            {
+                //Output "Normal"
+                System.out.println("Normal:\nYour BMI is: "+answerbmi+"\n");
+            }
+            //if NORMAL < answerbmi < OVERWEIGHT
+            if (NORMAL < answerbmi || answerbmi < OVERWEIGHT)
+            {
+                suggestedweight = weight - suggestedweightMax;
+                //Output "Overweight" and weight - suggestedweightMax
+                
+            }
+            //if answerbmi > OBESE
+            //Output "Obese" and weight - suggestedweightMax
+            //end while loop
+            }
+            //end while loop
         }
     }
 }
