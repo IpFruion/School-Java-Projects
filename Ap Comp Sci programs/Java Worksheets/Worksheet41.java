ea4065fe2f7bdee09a9c8fe2cfd5a0874814780a
BlackQuantum <lockwood.derrick@gmail.com>
2012-12-10 10:49:55 -0600

    Update Ap Comp Sci programs/Java Worksheets/Worksheet41.java

--- Ap Comp Sci programs/Java Worksheets/Worksheet41.java
+++ Ap Comp Sci programs/Java Worksheets/Worksheet41.java
@@ -2,32 +2,55 @@
 /**
  * Write a description of class Worksheet41 here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Derrick Lockwood 
+ * @version 12-10-12
  */
 public class Worksheet41
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Worksheet41
-     */
-    public Worksheet41()
+    int i,j,k;
+    double d1,d2,d3;
+    boolean b1,b2,b3;
+    public static void main()
     {
-        // initialise instance variables
-        x = 0;
+       
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
+    //problem 1
+    public boolean mercury(int i, int j)
     {
-        // put your code here
-        return x + y;
+        return b1;
+    }
+    //problem 2
+    public int venus(int k)
+    {
+        return k;
+    }
+    //problem 3
+    public double earth(double d1, double d2)
+    {
+        return d3;
+    }
+    //problem 4
+    public boolean mars(int j, double d1)
+    {
+        return b1;
+    }
+    //problem 5
+    public boolean jupiter()
+    {
+        return b1;
+    }
+    //problem 6
+    public double saturn(int i,  int j, double d1)
+    {
+        return d2;
+    }
+    //problem 7
+    public void uranus(double d1, double d2, double d3)
+    {
+    }
+    //problem 8
+    public boolean neptune(double d1, double d2, boolean b1)
+    {
+        return b1;
     }
 }
