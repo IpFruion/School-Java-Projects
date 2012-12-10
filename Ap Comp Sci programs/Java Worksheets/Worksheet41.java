eeea0641b0bd97af33eb0c1570c37f4a930be3a7
BlackQuantum <lockwood.derrick@gmail.com>
2012-12-10 10:53:58 -0600

    Update Ap Comp Sci programs/Java Worksheets/Worksheet41.java

--- Ap Comp Sci programs/Java Worksheets/Worksheet41.java
+++ Ap Comp Sci programs/Java Worksheets/Worksheet41.java
@@ -10,47 +10,21 @@ public class Worksheet41
     int i,j,k;
     double d1,d2,d3;
     boolean b1,b2,b3;
-    public static void main()
-    {
-       
-    }
+  
     //problem 1
     public boolean mercury(int i, int j)
-    {
-        return b1;
-    }
     //problem 2
     public int venus(int k)
-    {
-        return k;
-    }
     //problem 3
     public double earth(double d1, double d2)
-    {
-        return d3;
-    }
     //problem 4
     public boolean mars(int j, double d1)
-    {
-        return b1;
-    }
     //problem 5
     public boolean jupiter()
-    {
-        return b1;
-    }
     //problem 6
     public double saturn(int i,  int j, double d1)
-    {
-        return d2;
-    }
     //problem 7
     public void uranus(double d1, double d2, double d3)
-    {
-    }
     //problem 8
     public boolean neptune(double d1, double d2, boolean b1)
-    {
-        return b1;
-    }
 }
