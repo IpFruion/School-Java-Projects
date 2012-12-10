6d74934529ecc1aff79eb08e3720a80d1c81376a
BlackQuantum <lockwood.derrick@gmail.com>
2012-12-10 10:51:10 -0600

    Update Ap Comp Sci programs/Java Worksheets/Worksheet42.java

--- Ap Comp Sci programs/Java Worksheets/Worksheet42.java
+++ Ap Comp Sci programs/Java Worksheets/Worksheet42.java
@@ -2,32 +2,26 @@
 /**
  * Write a description of class Worksheet42 here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Derrick Lockwood 
+ * @version 12-10-12
  */
 public class Worksheet42
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Worksheet42
-     */
-    public Worksheet42()
+    public static void main()
     {
-        // initialise instance variables
-        x = 0;
+        int m,n,p;
+        double a1,a2,a3;
+        boolean c1,c2;
+        
+        n = mercury(m,c1);
+        c1 = venus(m);
+        a3 = earth(a1,a2);
+        c2 = mars(m,a1);
+        a1 = jupiter();
+        a2 = saturn(p, a1);
+        uranus(a1,a2,a3);
+        c2 = neptune(a1,n,a3);
+        
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    
 }
