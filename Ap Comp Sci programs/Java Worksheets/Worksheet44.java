0c0b92112f5f39e034c219bc35e02e957a130b0f
BlackQuantum <lockwood.derrick@gmail.com>
2012-12-13 10:59:03 -0600

    Update Ap Comp Sci programs/Java Worksheets/Worksheet44.java

--- Ap Comp Sci programs/Java Worksheets/Worksheet44.java
+++ Ap Comp Sci programs/Java Worksheets/Worksheet44.java
@@ -5,29 +5,47 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
+import java.util.Scanner;
 public class Worksheet44
 {
-    // instance variables - replace the example below with your own
-    private int x;
 
-    /**
-     * Constructor for objects of class Worksheet44
-     */
-    public Worksheet44()
+    public static void main()
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        Scanner stdin = new Scanner(System.in);
+        String input;
+        String input5;
+        String answer1 = "goodbye";
+        String yes = "yes";
+        for (int x = 0; x<10 ; x++)
+        {
+            System.out.println(Math.random());
+        }
+        for (int x = 10; x>0; x--)
+        {
+            System.out.println(Math.round(Math.random()*100));
+            //System.out.println(Math.round(Math.random()/10) *1000);
+        }
+        System.out.println("Enter a String: ");
+        System.out.print(stdin.next() + "\n");
+        System.out.println("Enter a Answer to problem :) : ");
+        input = stdin.next();
+        if (input.compareTo(answer1) == 0)
+        {
+            System.out.println("You say goodbye and I say hello!");
+        }
+        else
+        {
+            System.out.println("Hello, hello!");
+        }
+        while (true)
+        {
+            System.out.println(Math.round(Math.random()*1000));
+            System.out.println("Continue (yes/no)");
+            input5 = stdin.next();
+            if (input5.compareTo(yes) != 0)
+            {
+                break;
+            }
+        }
     }
 }
