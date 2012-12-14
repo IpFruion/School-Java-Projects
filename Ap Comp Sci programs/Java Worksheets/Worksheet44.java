8fe81917abf8c0dccc7217ac5988966d19ec97a8
BlackQuantum <lockwood.derrick@gmail.com>
2012-12-14 10:36:52 -0600

    Update Ap Comp Sci programs/Java Worksheets/Worksheet44.java

--- Ap Comp Sci programs/Java Worksheets/Worksheet44.java
+++ Ap Comp Sci programs/Java Worksheets/Worksheet44.java
@@ -11,22 +11,38 @@ public class Worksheet44
 
     public static void main()
     {
+        //problem definitions 
         Scanner stdin = new Scanner(System.in);
         String input;
         String input5;
         String answer1 = "goodbye";
         String yes = "yes";
+        String tempString;
+        double tempDouble;
+        int temp;
+        //problem 1
+        System.out.println("Problem 1");
         for (int x = 0; x<10 ; x++)
         {
-            System.out.println(Math.random());
+            tempDouble = Math.random();
+            System.out.println(tempDouble);
         }
+        //problem 2
+        System.out.println("Problem 2");
         for (int x = 10; x>0; x--)
         {
-            System.out.println(Math.round(Math.random()*100));
-            //System.out.println(Math.round(Math.random()/10) *1000);
+            tempDouble = Math.random();
+            tempDouble = tempDouble * 100;
+            temp = (int)Math.round(tempDouble);
+            System.out.println(temp);
         }
+        //problem 3
+        System.out.println("Problem 3");
         System.out.println("Enter a String: ");
-        System.out.print(stdin.next() + "\n");
+        tempString = stdin.next();
+        System.out.print(tempString + "\n");
+        //problem 4
+        System.out.println("Problem 4");
         System.out.println("Enter a Answer to problem :) : ");
         input = stdin.next();
         if (input.compareTo(answer1) == 0)
@@ -37,9 +53,14 @@ public class Worksheet44
         {
             System.out.println("Hello, hello!");
         }
+        //problem 5
+        System.out.println("Problem  5");
         while (true)
         {
-            System.out.println(Math.round(Math.random()*1000));
+            tempDouble = Math.random();
+            tempDouble = tempDouble * 1000;
+            temp = (int)Math.round(tempDouble);
+            System.out.println(temp);
             System.out.println("Continue (yes/no)");
             input5 = stdin.next();
             if (input5.compareTo(yes) != 0)
